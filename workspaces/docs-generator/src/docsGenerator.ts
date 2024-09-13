d71807779a6d666671914be30853cbcb0f3ca321
Stefan Vacareanu <stefan@codacy.com>
2024-09-13 13:42:18 +0200

    Fix doc generator

--- workspaces/docs-generator/src/docsGenerator.ts
+++ workspaces/docs-generator/src/docsGenerator.ts
@@ -223,32 +223,48 @@ export class DocsGenerator {
   }
 
   async createPatternDescriptionFile(
-    plugin: Plugin,
-    packageName: string,
-    pattern: string,
-    patternDocFilename: string
+      plugin: Plugin,
+      packageName: string,
+      pattern: string,
+      patternDocFilename: string
   ): Promise<void> {
     const docsInfo = this.docs[packageName];
     const rejectOnError = docsInfo.rejectOnError;
     const pluginVersion = (await this.dependencies)[plugin.packageName];
-    const url = (
-      (docsInfo.versionPrefix !== false && docsInfo.versionPrefix !== undefined)
-        ? docsInfo.baseUrl?.href.replace(/main|master/, `${docsInfo.versionPrefix}${pluginVersion}`)
-        : docsInfo.baseUrl?.href
+
+    // Check if the pattern needs URL adjustment. For @shopify in-folder rules: https://github.com/Shopify/web-configs/tree/main/packages/eslint-plugin/docs/rules
+    const needsAdjustment = [
+      "typescript-prefer-pascal-case-enums",
+      "typescript-prefer-singular-enums",
+      "jest-no-all-mocks-methods",
+      "typescript-prefer-build-client-schema",
+      "jest-no-snapshots",
+      "webpack-no-unnamed-dynamic-imports"
+    ].includes(pattern);
+
+    let url = (
+        (docsInfo.versionPrefix !== false && docsInfo.versionPrefix !== undefined)
+            ? docsInfo.baseUrl?.href.replace(/main|master/, `${docsInfo.versionPrefix}${pluginVersion}`)
+            : docsInfo.baseUrl?.href
     ) + patternDocFilename;
+
+    // Adjust the URL for specific patterns
+    if (needsAdjustment) {
+      // Replace the first hyphen with a slash
+      url = url.replace(/-(?=[^/]*\.md$)/, "/");
+    }
+
     try {
-
-
       const response = (await axios.get(url)).data as string;
       const text: string =
-        docsInfo.baseUrl
-          ? this.inlineLinkedMarkdownFiles(response, docsInfo.baseUrl.href)
-          : response;
+          docsInfo.baseUrl
+              ? this.inlineLinkedMarkdownFiles(response, docsInfo.baseUrl.href)
+              : response;
       const filePath = `${this.docsDescriptionDirectory}/${patternIdToCodacy((plugin.name !== "eslint" ? plugin.name + "/" : "") + pattern)}.md`;
 
       await writeFile(filePath, plugin.name === "@typescript-eslint" ? this.convertMdxToMd(text) : text);
     } catch (error) {
-      console.log({plugin: plugin.name, url})
+      console.log({ plugin: plugin.name, url });
       const message = `Failed to retrieve docs for ${pattern}\n- ${error}`;
 
       if (rejectOnError) {
@@ -257,6 +273,7 @@ export class DocsGenerator {
       console.error(wrapConsoleTextInColor(message, TerminalColor.Red));
     }
   }
+
 
   async downloadAllPluginsDocs(downloadDocsInfo: DocsInfo[]): Promise<void> {
     downloadDocsInfo.forEach((docsInfo) => {
