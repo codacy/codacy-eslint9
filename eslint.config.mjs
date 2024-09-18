e7717c65212e126d941d3dc3b936aa94367c7844
Stefan Vacareanu <stefan@codacy.com>
2024-09-18 14:22:38 +0200

    fix config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,40 +6,39 @@ import tseslint from 'typescript-eslint';
 //import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  {
-    // config with just ignores is the replacement for `.eslintignore`
-    ignores: ['dist/**'],
-  },
-  //prettier,
-  eslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    plugins: ['@typescript-eslint'],
-    languageOptions: {
-      parser: tseslint.parser,
-      globals: globals.node,
-      parserOptions: {
-        project: true,
-        sourceType: 'module',
-        programs: null
+    {
+      // Config with just ignores is the replacement for `.eslintignore`
+      ignores: ['dist/**'],
+    },
+    // prettier,
+    eslint.configs.recommended,
+    {
+      files: ['**/*.ts'],
+      languageOptions: {
+        parser: tseslint.parser,
+        globals: globals.node,
+        parserOptions: {
+          project: true,
+          sourceType: 'module',
+          programs: null,
+        },
+      },
+      rules: {
+        '@typescript-eslint/no-unsafe-argument': 'error',
+        '@typescript-eslint/no-unsafe-assignment': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/no-unsafe-member-access': 'error',
+        '@typescript-eslint/no-unsafe-return': 'error',
       },
     },
-    rules: {
-      '@typescript-eslint/no-unsafe-argument': 'error',
-      '@typescript-eslint/no-unsafe-assignment': 'error',
-      '@typescript-eslint/no-unsafe-call': 'error',
-      '@typescript-eslint/no-unsafe-member-access': 'error',
-      '@typescript-eslint/no-unsafe-return': 'error',
+    {
+      // Disable type-aware linting on JS files
+      files: ['**/*.js'],
+      ...tseslint.configs.disableTypeChecked,
     },
-  },
-  {
-    // disable type-aware linting on JS files
-    files: ['**/*.js'],
-    ...tseslint.configs.disableTypeChecked,
-  },
-  // {
-  //   // enable jest rules on test files
-  //   files: ['tests/**'],
-  //   ...jestPlugin.configs['flat/recommended'],
-  // },
+    // {
+    //   // Enable Jest rules on test files
+    //   files: ['tests/**'],
+    //   ...jestPlugin.configs['flat/recommended'],
+    // },
 );
