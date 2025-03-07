fbeced55cb289eb2739e3b502b549cffc9953571
DMarinhoCodacy <102817200+DMarinhoCodacy@users.noreply.github.com>
2025-03-07 12:34:51 +0000

    improve entrypoing.sh within node command (#45)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,4 @@ max_ram=$((max_ram / 1024 / 1024))
 old_space_size=$((max_ram - 356))
 
 export TS_NODE_PROJECT=/app/workspaces/codacy/tsconfig.json
-
-exec node --max-old-space-size="$old_space_size" --max-semi-space-size=16 --optimize_for_size --gc_interval=100 --v8-pool-size=0 --use-largepages=silent --import=/app/register.js /app/workspaces/codacy/src/index.ts
+exec node --max-old-space-size="$old_space_size" --max-semi-space-size=16 --gc_interval=100 --v8-pool-size=0 --use-largepages=silent --import=/app/register.js /app/workspaces/codacy/src/index.ts
